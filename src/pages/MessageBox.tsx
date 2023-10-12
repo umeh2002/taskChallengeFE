e72bac6a31f1a6e8bd94ac0af3d2741609cd84e9
umeh emmanuel <eumeh3882@gmail.com>
2023-10-12 00:10:09 -0700

    emma

--- src/pages/MessageBox.tsx
+++ src/pages/MessageBox.tsx
@@ -3,7 +3,7 @@
 const MessageBox = () => {
   return (
     <div className="w-full h-[100vh] flex justify-center items-center">
-        <div className="w-[90%] h-full flex justify-center items-center">
+        <div className="w-[90%] h-full flex justify-center items-center flex-col">
             <div className="text-[30px] mb-2">congratulation</div>
             <div className="text-[20px] ">you have successfully register to our platform, kindly go to your email to verify </div>
         </div>
